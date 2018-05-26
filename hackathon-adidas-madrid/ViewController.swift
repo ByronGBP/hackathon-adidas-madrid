f07d30f9ca851669d59f38ed9c2e6e5890fac1c9
ByronGBP <byrongbp@gmail.com>
2018-05-26 11:02:21 +0200

    setup

---
+++ hackathon-adidas-madrid/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hackathon-adidas-madrid
+//
+//  Created by Byron Bacusoy Pinela on 26/5/18.
+//  Copyright © 2018 Byron Bacusoy Pinela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
